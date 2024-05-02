75045faae609f9c1100547a843e8e0f9ec1bb165
E <le.elton1@gmail.com>
2024-05-02 06:39:19 -0700

    hw?

---
+++ Java Classwork/Java-Classwork-1/src/main/java/org/example/Homework303_2_1.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class Homework303_2_1 {
+    public static void main(String[] args) {
+
+        //Write a program that declares two integer variables, assigns an integer to each, and adds them together.
+        // Assign the sum to a variable. Print out the result.
+        int num1 =10;
+        int num2 =20;
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+        // Write a program that declares two double variables, assings a number to each, and adds them together.
+        // Assign the sum to a variable. print out the result.
+        double d1 = 10.5d;
+        double d2 = 20.5d;
+
+        double dSum = d1 + d2;
+        System.out.println(dSum);
+
+        // Write a program that declares an integer variable and a double variable, assigns numbers to each, and adds
+        // Assign the sum to a variable. Print out the result. What variable type must the sum be?
+        int x = 30;
+        double y = 50.55;
+
+        double sum3 = x + y;
+        System.out.println(sum3);
+
+        //Write a program that declares two integer variables
+        //Assign the result
+        //What happens? What corrections are needed?
+        int v1 = 4;
+        int v2 = 2;
+
+        double div = (double)v1 / (double)v2;
+        System.out.println(div);
+
+        //Write a program that declares two integer variables, x and y, and assigns the number 5 to x and the number 6 to y
+        //Declare a variable q and assign y/x to it and print q. Now, cast y to a double and assign it to q. Print q again.
+
+        int x = 5;
+        int y = 6;
+        double q = y/x;
+        System.out.println(q);
+
+        //Write a program that declares a named constant and uses it in a calculation.
+
+
+
+
+    }
+}
